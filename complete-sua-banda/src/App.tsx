5d9b814343e52bb555a65e0c3e421c3971e69923
rogeraguiar0 <aguiarroger02@gmail.com>
2022-11-02 15:24:07 -0300

    fix: remove app.tsx waste

--- complete-sua-banda/src/App.tsx
+++ complete-sua-banda/src/App.tsx
@@ -1,18 +1,9 @@
 import { GlobalStyle } from "./styles/GlobalStyles";
-import { useForm } from "react-hook-form";
-import { Input } from "./components/Input";
 
 const App = () => {
-  const { handleSubmit, register } = useForm();
-
-  const handleForm = handleSubmit((data) => console.log(data));
-
   return (
     <>
       <GlobalStyle />
-      <form onSubmit={handleForm}>
-        <Input name="email" register={register} />
-      </form>
     </>
   );
 };
