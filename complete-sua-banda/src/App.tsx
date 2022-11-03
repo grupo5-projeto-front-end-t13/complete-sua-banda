e88c2ec92418368f5a35f16fcce47c6aa97d4758
rogeraguiar0 <aguiarroger02@gmail.com>
2022-11-03 11:35:47 -0300

    feat: new styles and add icon props

--- complete-sua-banda/src/App.tsx
+++ complete-sua-banda/src/App.tsx
@@ -1,9 +1,24 @@
 import { GlobalStyle } from "./styles/GlobalStyles";
+import { useForm } from "react-hook-form";
+import { Input } from "./components/Input";
+import { AiFillAccountBook } from "react-icons/ai";
 
 const App = () => {
+  const { handleSubmit, register } = useForm();
+
+  const handleForm = handleSubmit((data) => console.log(data));
+
   return (
     <>
       <GlobalStyle />
+      <form onSubmit={handleForm}>
+        <Input
+          register={register}
+          type="email"
+          name="email"
+          icon={<AiFillAccountBook />}
+        />
+      </form>
     </>
   );
 };
