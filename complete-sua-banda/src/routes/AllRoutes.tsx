63cd693267261d6a0b28f9240ee8743af90eed39
Bruno Paulin <bruno.p-@hotmail.com>
2022-11-05 11:55:45 -0300

    feat: add dashboard page && add modalCard component

--- complete-sua-banda/src/routes/AllRoutes.tsx
+++ complete-sua-banda/src/routes/AllRoutes.tsx
@@ -3,7 +3,7 @@ import { LandingPage } from "../pages/LandingPage";
 import { LoginPage } from "../pages/LoginPage";
 import { RegisterBand } from "../pages/RegisterBand";
 import { RegisterMusician } from "../pages/RegisterMusician";
-// // import { DashboardBand } from "../pages/DashboardBand";
+import { DashboardBand } from "../pages/DashboardBand";
 // // import { DashboardMusician } from "../pages/DashboardMusician";
 
 export const AllRoutes = () => {
@@ -13,8 +13,8 @@ export const AllRoutes = () => {
       <Route path="/login" element={<LoginPage />} />
       <Route path="/registerBand" element={<RegisterBand />} />
       <Route path="/registerMusician" element={<RegisterMusician />} />
-      {/* <Route path="/dashboardBand" element={<DashboardBand />} />
-      <Route path="/dashboardMusician" element={<DashboardMusician />} /> */}
+      <Route path="/dashboardBand" element={<DashboardBand />} />
+      {/* <Route path="/dashboardMusician" element={<DashboardMusician />} /> */}
       <Route path="*" element={<Navigate to={"/"} />} />
     </Routes>
   );
