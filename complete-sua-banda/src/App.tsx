2e26fdb59a3636c710704230e3fa39667f10d617
rogeraguiar0 <aguiarroger02@gmail.com>
2022-11-05 19:33:21 -0300

    feat: animations to landingpage, login and register

--- complete-sua-banda/src/App.tsx
+++ complete-sua-banda/src/App.tsx
@@ -1,31 +1,14 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 // import { RegisterBand } from "./pages/RegisterBand";
 import { AllRoutes } from "./routes/AllRoutes";
-=======
-import { RegisterBand } from "./pages/RegisterBand";
-import { RegisterMusician } from "./pages/RegisterMusician";
->>>>>>> 4e2016f6945c2c2779ef1c5dfe954ccf776ef541
-=======
-import { RegisterBand } from "./pages/RegisterBand";
-import { RegisterMusician } from "./pages/RegisterMusician";
->>>>>>> 2904fdd482d35f6b206bd4efe16c6037fe7d457d
 import { GlobalStyle } from "./styles/GlobalStyles";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { AuthProvider } from "./context/AuthContext";
-import { AllRoutes } from "./routes/AllRoutes";
 
 const App = () => {
   return (
     <>
       <GlobalStyle />
-<<<<<<< HEAD
-<<<<<<< HEAD
-      <AllRoutes />
-=======
-=======
->>>>>>> 2904fdd482d35f6b206bd4efe16c6037fe7d457d
       <ToastContainer
         position="top-right"
         autoClose={2000}
@@ -38,10 +21,6 @@ const App = () => {
       <AuthProvider>
         <AllRoutes />
       </AuthProvider>
-<<<<<<< HEAD
->>>>>>> 4e2016f6945c2c2779ef1c5dfe954ccf776ef541
-=======
->>>>>>> 2904fdd482d35f6b206bd4efe16c6037fe7d457d
     </>
   );
 };
